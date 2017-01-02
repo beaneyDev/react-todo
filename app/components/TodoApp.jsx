382bd12e906640c7592c2a92094ac4ad6c278410
Matt Beaney <matt.beaney@pagesuite.co.uk>
2017-01-02 16:13:56 +0000

    Added todo item and list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var Todo = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function () {
+      return {
+        todos: [
+          {
+            id: 1,
+            text: 'Walk the dog'
+          },
+          {
+            id: 2,
+            text: 'Clean the yard'
+          },
+          {
+            id: 3,
+            text: 'Beaney'
+          },
+          {
+            id: 4,
+            text: 'Weeney'
+          }
+        ]
+      }
+    },
     render: function () {
+      var {todos} = this.state;
       return (
         <div>
-          TodoApp.jsx
+          <Todo todos={todos}/>
         </div>
       )
     }
