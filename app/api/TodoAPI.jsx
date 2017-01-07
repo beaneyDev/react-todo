95e1910fe2e73e7aa3b25ae8a5b27fb38234bbcc
Matt Beaney <matt.beaney@pagesuite.co.uk>
2017-01-07 18:52:26 +0000

    added search functionality and tests

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -22,5 +22,32 @@ module.exports = {
 
     //Check its an actual array, if not return empty array.
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by search text
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    })
+
+    //Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if(a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -32,7 +32,7 @@ describe("TodoAPI", () => {
       };
 
       TodoAPI.setTodos(badTodos);
-      
+
       var actualTodos = JSON.parse(localStorage.getItem('todos'));
       expect(actualTodos).toBe(null);
     })
@@ -65,5 +65,59 @@ describe("TodoAPI", () => {
       var storedTodos = TodoAPI.getTodos();
       expect(storedTodos).toEqual(todos);
     });
+  });
+
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'Some text here',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'Other text here',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'Some text here',
+        completed: true
+      }
+    ];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      //Tutorial said to rely on length, I don't like it.
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should only return all incomplete items', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      //Tutorial said to rely on length, I don't like it.
+      expect(filteredTodos.length).toBe(1);
+
+      //This feels more robust and will outlast me changing the dummy data.
+      filteredTodos.forEach((todo) => {
+        expect(todo.completed).toBe(false);
+      });
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter by search term', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todo items if empty string passed in', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    })
   })
 });
