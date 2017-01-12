3adeeffb6690425bc023a27e9b25ab3b1a227f29
Matt Beaney <matt.beaney@pagesuite.co.uk>
2017-01-12 20:18:46 +0000

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 //Components
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Walk the dog'));
+store.dispatch(actions.setSearchText('Yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 //Load foundation and styles
 require('style!css!sass!applicationStyles')
 
